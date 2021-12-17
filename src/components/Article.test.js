01a9a8173082f780b7fb7af1499388d768771efe
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-12-17 15:56:30 -0500

    pass all tests on article.test.js

--- src/components/Article.test.js
+++ src/components/Article.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import MutationObserver from 'mutationobserver-shim';
 
@@ -49,16 +49,18 @@ test('renders "Associated Press" when no author is given', () => {
   expect(author).toHaveTextContent(/by associated press/i);
 });
 
-// test('executes handleDelete when the delete button is pressed', () => {
-//   const mockHandleDelete = jest.fn();
+test('executes handleDelete when the delete button is pressed', async () => {
+  const mockHandleDelete = jest.fn();
 
-//   render(<Article handleDelete={mockHandleDelete}/>);
+  render(<Article article={testArticle} handleDelete={mockHandleDelete}/>);
 
-//   const button = screen.queryByTestId('deleteButton');
-//   userEvent.click(button);
+  const button = screen.queryByTestId('deleteButton');
+  userEvent.click(button);
 
-//   expect(handleDelete).toHaveBeenCalled();
-// });
+  await waitFor(() => {
+    expect(mockHandleDelete).toHaveBeenCalled();
+  })
+});
 
 //Task List:
 //1. Complete all above tests. Create test article data when needed.
